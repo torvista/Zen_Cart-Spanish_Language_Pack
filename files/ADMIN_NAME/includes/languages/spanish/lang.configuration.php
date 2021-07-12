a985471a05f2a538f99a63086479ffa7868f4016
Steve <4391026+torvista@users.noreply.github.com>
2021-07-12 08:55:41 +0200

    add configuration page titles

--- files/ADMIN_NAME/includes/languages/spanish/lang.configuration.php
+++ files/ADMIN_NAME/includes/languages/spanish/lang.configuration.php
@@ -1,5 +1,5 @@
 <?php // Spanish Language Pack for Zen Cart: https://github.com/torvista/Zen_Cart-Spanish_Language_Pack
-/**
+/** spanish: added overrides for page titles from group name constants
  * @copyright Copyright 2003-2020 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
@@ -7,8 +7,6 @@
 */
 
 $define = [
-    'CFGTITLE_STORE_NAME' => 'Nombre de la Tienda',//example override for configuration constant titles
-    'CFGDESC_STORE_NAME' =>'El nombre de mi tienda',//example override for configuration constant description
     'TABLE_HEADING_CONFIGURATION_TITLE' => 'Título',
     'TABLE_HEADING_CONFIGURATION_VALUE' => 'Valor',
     'TABLE_HEADING_ACTION' => 'Acción',
@@ -78,6 +76,35 @@ $define = [
     'TEXT_MAX_ADMIN_DISPLAY_PRODUCTS_TO_CATEGORIES_COLUMNS_LENGTH' => 'El valor debe ser un integer.',
     'TEXT_MAX_ADMIN_DISPLAY_SEARCH_RESULTS_EZPAGE_LENGTH' => 'El valor debe ser un integer.',
     'TEXT_MAX_PREVIEW' => 'El valor debe ser un integer.',
+//bof constant page titles
+    'CFG_GRP_TITLE_MY_STORE' => '%%BOX_CONFIGURATION_MY_STORE%%',
+    'CFG_GRP_TITLE_MINIMUM_VALUES' => '%%BOX_CONFIGURATION_MINIMUM_VALUES%%',
+    'CFG_GRP_TITLE_MAXIMUM_VALUES' => '%%BOX_CONFIGURATION_MAXIMUM_VALUES%%',
+    'CFG_GRP_TITLE_IMAGES' => '%%BOX_CONFIGURATION_IMAGES%%',
+    'CFG_GRP_TITLE_CUSTOMER_DETAILS' => '%%BOX_CONFIGURATION_CUSTOMER_DETAILS%%',
+    'CFG_GRP_TITLE_SHIPPING_PACKAGING' => '%%BOX_CONFIGURATION_SHIPPING_PACKAGING%%',
+    'CFG_GRP_TITLE_PRODUCT_LISTING' => '%%BOX_CONFIGURATION_PRODUCT_LISTING%%',
+    'CFG_GRP_TITLE_STOCK' => '%%BOX_CONFIGURATION_STOCK%%',
+    'CFG_GRP_TITLE_LOGGING' => '%%BOX_CONFIGURATION_LOGGING%%',
+    'CFG_GRP_TITLE_EMAIL_OPTIONS' => '%%BOX_CONFIGURATION_EMAIL_OPTIONS%%',
+    'CFG_GRP_TITLE_ATTRIBUTE_OPTIONS' => '%%BOX_CONFIGURATION_ATTRIBUTE_OPTIONS%%',
+    'CFG_GRP_TITLE_GZIP_COMPRESSION' => '%%BOX_CONFIGURATION_GZIP_COMPRESSION%%',
+    'CFG_GRP_TITLE_SESSIONS' => '%%BOX_CONFIGURATION_SESSIONS%%',
+    'CFG_GRP_TITLE_REGULATIONS' => '%%BOX_CONFIGURATION_REGULATIONS%%',
+    'CFG_GRP_TITLE_GV_COUPONS' => '%%BOX_CONFIGURATION_GV_COUPONS%%',
+    'CFG_GRP_TITLE_CREDIT_CARDS' => '%%BOX_CONFIGURATION_CREDIT_CARDS%%',
+    'CFG_GRP_TITLE_PRODUCT_INFO' => '%%BOX_CONFIGURATION_PRODUCT_INFO%%',
+    'CFG_GRP_TITLE_LAYOUT_SETTINGS' => '%%BOX_CONFIGURATION_LAYOUT_SETTINGS%%',
+    'CFG_GRP_TITLE_WEBSITE_MAINTENANCE' => '%%BOX_CONFIGURATION_WEBSITE_MAINTENANCE%%',
+    'CFG_GRP_TITLE_NEW_LISTING' => '%%BOX_CONFIGURATION_NEW_LISTING%%',
+    'CFG_GRP_TITLE_FEATURED_LISTING' => '%%BOX_CONFIGURATION_FEATURED_LISTING%%',
+    'CFG_GRP_TITLE_ALL_LISTING' => '%%BOX_CONFIGURATION_ALL_LISTING%%',
+    'CFG_GRP_TITLE_INDEX_LISTING' => '%%BOX_CONFIGURATION_INDEX_LISTING%%',
+    'CFG_GRP_TITLE_DEFINE_PAGE_STATUS' => '%%BOX_CONFIGURATION_DEFINE_PAGE_STATUS%%',
+    'CFG_GRP_TITLE_EZPAGES_SETTINGS' => '%%BOX_CONFIGURATION_EZPAGES_SETTINGS%%',
+//eof page titles    
+    'CFGTITLE_STORE_NAME' => 'Nombre de la Tienda',  //example override for configuration constant title
+    'CFGDESC_STORE_NAME' =>'El nombre de mi tienda',   //example override for configuration constant description    
 ];
 
 return $define;
