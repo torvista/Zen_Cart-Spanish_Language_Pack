0888e7db657dfd18445d65cd28bf03aff9fec4fe
torvista <4391026+torvista@users.noreply.github.com>
2022-02-27 09:29:17 +0100

    update

--- files/includes/languages/spanish/lang.audience.php
+++ files/includes/languages/spanish/lang.audience.php
@@ -1,6 +1,7 @@
 <?php //https://github.com/torvista/Zen_Cart-Spanish_Language_Pack
-if (!defined('TEXT_SELECT_AN_OPTION')) $define = [
-    'TEXT_SELECT_AN_OPTION' => 'Elija una opción',
-];
+$define = []; 
+if (!defined('TEXT_SELECT_AN_OPTION')) {
+    $define['TEXT_SELECT_AN_OPTION'] = 'Elija una opción',
+}
 
 return $define;
