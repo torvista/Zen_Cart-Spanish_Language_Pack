ed95e0b7ef46cb096823fe61e9215498254a7718
torvista <4391026+torvista@users.noreply.github.com>
2022-02-28 22:41:49 +0100

    version

--- files/ADMIN_NAME/includes/languages/spanish/developers_tool_kit.php
+++ files/ADMIN_NAME/includes/languages/spanish/developers_tool_kit.php
@@ -1,9 +1,9 @@
 <?php //Spanish Language Pack for Zen Cart 1.5x: https://github.com/torvista/Zen-Cart-1.5x-Spanish-Language-Pack
 /**
- * @copyright Copyright 2003-2020 Zen Cart Development Team
+ * @copyright Copyright 2003-2022 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: DrByte 2020 Jul 21 Modified in v1.5.7a $
+ * @version $Id: mc12345678 2021 Nov 20 Modified in v1.5.7d $
  */
   define('HEADING_TITLE', 'Buscadores de Código Fuente ');
   define('TABLE_CONFIGURATION_TABLE', 'Buscar definiciones de CONSTANTS');
