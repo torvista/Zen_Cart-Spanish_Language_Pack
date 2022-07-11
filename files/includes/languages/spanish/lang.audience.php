92a644b33e8d922fe4a212fba36459023c69a764
torvista <4391026+torvista@users.noreply.github.com>
2022-07-11 13:28:29 +0200

    fix comma to semicolon

--- files/includes/languages/spanish/lang.audience.php
+++ files/includes/languages/spanish/lang.audience.php
@@ -1,7 +1,7 @@
 <?php //https://github.com/torvista/Zen_Cart-Spanish_Language_Pack
 $define = []; 
 if (!defined('TEXT_SELECT_AN_OPTION')) {
-    $define['TEXT_SELECT_AN_OPTION'] = 'Elija una opción',
+    $define['TEXT_SELECT_AN_OPTION'] = 'Elija una opción';
 }
 
 return $define;
