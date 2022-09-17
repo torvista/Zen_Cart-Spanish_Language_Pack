964e4fcede6082a8858c33e16f8dc9eafc30965d
torvista <4391026+torvista@users.noreply.github.com>
2022-09-17 13:01:42 +0200

    update Display Logs

--- files/zc_plugins/DisplayLogs/v3.0.1/admin/includes/languages/spanish/extra_definitions/lang.display_logs_name.php
+++ files/zc_plugins/DisplayLogs/v3.0.1/admin/includes/languages/spanish/extra_definitions/lang.display_logs_name.php
@@ -8,7 +8,7 @@
 
 $define = [
       'BOX_TOOLS_DISPLAY_LOGS' => 'Mostrar Debug-logs',
-      'DISPLAY_LOGS_MESSAGE_LOGS_PRESENT' => 'Hay %1$u Debug-logs, pinche <a href="%2$s">aqui</a> para visualizarlos.',
+      'DISPLAY_LOGS_MESSAGE_LOGS_PRESENT' => 'Hay %1$u Debug-logs, pinche <a href="%2$s">aquí</a> para visualizarlos.',
 ];
 
 return $define;
