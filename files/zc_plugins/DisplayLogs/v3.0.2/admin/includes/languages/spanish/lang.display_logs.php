8df79b464a8816a8c5421f9ee223e962165740f4
Steve <4391026+torvista@users.noreply.github.com>
2023-01-15 13:20:31 +0100

    display logs 3.0.2

---
+++ files/zc_plugins/DisplayLogs/v3.0.2/admin/includes/languages/spanish/lang.display_logs.php
@@ -0,0 +1,38 @@
+<?php // Spanish Language Pack for Zen Cart: https://github.com/torvista/Zen_Cart-Spanish_Language_Pack
+/**
+ * @copyright Copyright 2003-2022 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: Steve 2021 Apr 02 New in v1.5.8-alpha $
+*/
+
+
+$define = [
+    'HEADING_TITLE' => 'Mostrar los Debug-logs',
+    'TABLE_HEADING_FILENAME' => 'Nombre Archivo',
+    'TABLE_HEADING_MODIFIED' => 'Fecha Modificado',
+    'TABLE_HEADING_FILESIZE' => 'Tamaño (bytes)',
+    'TABLE_HEADING_DELETE' => '¿Borrar?',
+    'TABLE_HEADING_ACTION' => 'Acción',
+    'BUTTON_INVERT_SELECTED' => 'Invertir Seleccción',
+    'BUTTON_DELETE_SELECTED' => 'Borrar Seleccionado(s)',
+    'DELETE_SELECTED_ALT' => 'Borrar Todos los archivos seleccionados',
+    'BUTTON_DELETE_ALL' => 'Borrar Todos',
+    'DELETE_ALL_ALT' => 'Borrar todos los archivos mostrados',
+    'ICON_INFO_VIEW' => 'Ver el contenido de este archivo',
+    'DISPLAY_DEBUG_LOGS_ONLY' => '¿Mostrar solamente los debugs?',
+    'TEXT_HEADING_INFO' => 'Contenido',
+    'TEXT_MOST_RECENT' => 'más reciente',
+    'TEXT_OLDEST' => 'más antiguo',
+    'TEXT_SMALLEST' => 'menor',
+    'TEXT_LARGEST' => 'mayor',
+    'TEXT_INSTRUCTIONS' => '<br><br>Se puede ordenar los archivos pinchando en el título de una columna. Pinche en el icono %7s para ver el contenido del archivo. Solamente muestra los primeros %1$u bytes; si el tamaño del archivo es &quot;excesivo&quot;, destaca <em>Tamaño Archivo</em> como <span class="bigfile">este</span>.<br><br>Pinche el botón <strong>Borrar Todo</strong> para borrar todos los archivos actualmente a la vista; pinche <strong>Borrar Seleccionado(s)</strong> para borrar los archivos marcados.<br><br>Actualmente mostrando el %2$s %3$u de %4$u archivos con el prefijo <code>%5$s</code> y <b>no</b> los que coinciden con <code>%6$s</code>.<br>',
+    'JS_MESSAGE_DELETE_ALL_CONFIRM' => '¿Está seguro que quiere borrar estos \'+n+\' archivos?',
+    'JS_MESSAGE_DELETE_SELECTED_CONFIRM' => '¿Está seguro que quiere borrar los \'+selected+\' archivo(s) seleccionados?',
+    'WARNING_NOT_SECURE' => '<span class="errorText">NOTA: SSL no habilitado. El contenido del archivo que visualiza en esta página no será encriptado y puede ser un riesgo de seguridad.</span>',
+    'WARNING_NO_FILES_SELECTED' => '¡No había archivos seleccionados para borrar!',
+    'WARNING_SOME_FILES_DELETED' => 'Aviso: Solamente se borraron %u de %u archivos; revise los permisos de los archivos/directorio.',
+    'SUCCESS_FILES_DELETED' => 'Se borró %u archivos.',
+];
+
+return $define;
