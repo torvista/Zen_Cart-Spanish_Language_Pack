dc9c2a78d4f8b1513591079b05eaef252006b08e
torvista <4391026+torvista@users.noreply.github.com>
2024-10-20 18:45:01 +0200

    update

--- files/ADMIN_FOLDER/includes/languages/spanish/lang.plugin_manager.php
+++ files/ADMIN_FOLDER/includes/languages/spanish/lang.plugin_manager.php
@@ -3,7 +3,7 @@
  * @copyright Copyright 2003-2024 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: Zcwilt 2023 Jul 01 Modified in v2.0.0-alpha1 $
+ * @version $Id: DrByte 2024 May 27 Modified in v2.1.0-alpha1 $
 */
 
 $define = [
@@ -19,7 +19,7 @@ $define = [
     'TEXT_CLEANUP' => 'Limpiar',
     'TEXT_UPGRADE' => 'Actualizar',
     'TEXT_CONFIRM' => 'Confirmar',
-    'TEXT_INFO_DESCRIPTION' => 'Descripción Plugin:',
+    'TEXT_INFO_DESCRIPTION' => '<strong>Descripción Plugin:</strong>',
     'TEXT_INFO_CLEANUP' => 'Borrar las carpetas innecesarias de las versiones',
     'TEXT_INFO_SELECT_CLEAN' => 'Seleccionar las versiones para borrar',
     'TEXT_INSTALLED_ENABLED' => 'Instalado (Habilitado)',
@@ -34,8 +34,10 @@ $define = [
     'TEXT_CLEANUP_SUCCESS' => 'Directorios borrados con éxito',
     'TEXT_CLEANUP_ERROR' => 'Revise los permisos de los archivos. No se borraron algunos directorios',
     'TEXT_UPGRADE_AVAILABLE' => 'Actualización Disponible',
-    'TEXT_VERSION_INSTALLED' => 'Versión Instalado: %s',
-    'TEXT_NEW_PLUGIN_DOWNLOAD_AVAILABLE' => 'Una nueva versión %1$s está disponible para descargar desde el <a target="_blank" href="https://www.zen-cart.com/downloads.php?do=file&id=%2$s">foro de soporte</a >',
+    'TEXT_VERSION_INSTALLED' => '<strong>Versión Instalado:</strong> %s',
+    'TEXT_PLUGIN_AUTHOR' => '<strong>Autor:</strong> %s',
+    'TEXT_PLUGIN_DOWNLOAD_PAGE' => '<a target="_blank" rel="noreferrer" href="https://www.zen-cart.com/downloads.php?do=file&id=%s">Página de Descarga (Zen Cart)</a>',
+    'TEXT_NEW_PLUGIN_DOWNLOAD_AVAILABLE' => 'Una nueva versión %1$s está disponible para descargar desde el <a target="_blank" rel="noreferrer" href="https://www.zen-cart.com/downloads.php?do=file&id=%2$s">foro de soporte</a >',
     'TEXT_CONFIRM_UNINSTALL' => '¿Está seguro de que quiere desinstalar este Plugin?',
     'TEXT_CONFIRM_UPGRADE' => '¿Está seguro de que quiere actualizar este Plugin?',
     'TEXT_CONFIRM_DISABLE' => '¿Está seguro de que quiere deshabilitar este Plugin?',
@@ -45,7 +47,7 @@ $define = [
     'TEXT_INFO_UPGRADE_WARNING' => '',
     'TEXT_INFO_CONFIRM_CLEAN' => 'Confirmar las carpetas de las versiones que quiere borrar',
     'TEXT_LABEL_STATUS' => 'Estatus: ',
-    'ERROR_NOT_FOUND_IN_SQL_FUNCTIONS_MAP' => 'Revise la petición SQL. No se encuentra un mapeo a la función sql para : ',
+    'ERROR_NOT_FOUND_IN_SQL_FUNCTIONS_MAP' => 'Revise la petición SQL. No se encuentra un mapeo a la función SQL para : ',
     'ERROR_INVALID_SYNTAX' => 'No se puede identificar la tabla porque el sintaxis es invalido en: ',
     'ERROR_SQL_PATCH' => 'Hubo un error en procesar el SQL. ',
 ];
