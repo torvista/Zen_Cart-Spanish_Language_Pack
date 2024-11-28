5ac000f6a2b5df60b24c2ae918490a12b4d4b3fe
torvista <4391026+torvista@users.noreply.github.com>
2024-11-28 09:57:47 +0100

    correct errant => characters

--- files/ADMIN_FOLDER/includes/languages/spanish/lang.admin_activity.php
+++ files/ADMIN_FOLDER/includes/languages/spanish/lang.admin_activity.php
@@ -12,7 +12,7 @@ $define = [
     'HEADING_SUB2' => 'Limpiar el historial de archivos',
     'TEXT_ACTIVITY_EXPORT_FORMAT' => 'Formato de Archivo de Exportación:',
     'TEXT_ACTIVITY_EXPORT_FILENAME' => 'Nombre de Archivo de Exportación:',
-    'TEXT_ACTIVITY_EXPORT_SAVETOFILE' => '¿Guardar en el servidor? (si no => se descargará directamente desde esta ventana)',
+    'TEXT_ACTIVITY_EXPORT_SAVETOFILE' => '¿Guardar en el servidor? (si no, se descargará directamente desde esta ventana)',
     'TEXT_ACTIVITY_EXPORT_DEST' => 'Destino: ',
     'SUCCESS_EXPORT_ADMIN_ACTIVITY_LOG' => 'Exportación completada: ',
     'FAILURE_EXPORT_ADMIN_ACTIVITY_LOG' => 'AVISO: La exportación falló. No se pudo escribir el archivo',
@@ -23,10 +23,10 @@ Debe archivar esta información por si acaso es requerida en una investigación 
 <li>Elija mostrar o exportar un archivo.</li>
 <li>Escriba un nombre para el archivo (debe tener una extensión .csv .txt .htm .html .xml).</li>
 <li>Pulse en Guardar para seguir.</li>
-<li>Elija si quiere guardar o abrir el archivo => dependiendo de lo que ofrece el navegador.</li></ol>
+<li>Elija si quiere guardar o abrir el archivo. dependiendo de lo que ofrece el navegador.</li></ol>
 ',
-    'TEXT_INFO_ADMIN_ACTIVITY_LOG' => '<strong>Tabla del Archivo de Admin vacío en la base de datos<br>¡AVISO: ASEGÚRESE DE HACER UNA COPIA DE SEGURIDAD DE LA BASE DE DATOS ANTES DE INICIAR ESTA ACTUALIZACIÓN!</strong><br>El Archivo de Actividades Admin es un método para grabar las actividades en el Admin. <br>Puede aumentar en tamaño rápidamente; hay que vaciarlo a menudo.<br>Salen avisos cuando la tabla alcanza 50.000 actividades o han pasado 60 días.<br><span class="alert">NOTA: Para cumplir con PCI => debe guardar estos datos de actividad durante 12 meses.<br>Recomendamos que guarde un copia - EXPORTAR A CSV => y GUARDAR - antes de limpiar el archivo.</span>',
-    'TEXT_ADMIN_LOG_PLEASE_CONFIRM_ERASE' => '<strong><span class="alert">¡PRECAUCIÓN! Se dispone a BORRAR entradas *importantes* de las Actividades de Admin de la base de datos.</span></strong><br>Asegúrese de que tiene un backup de estas entradas ANTES de borrarlas.<br>Si continúa => ud. acepta que está borrando las entradas y que entiende sus responsabilidades legales con respecto a estas entradas.<br><br>Entiendo mis responsabilidades y quiero borrar los datos cuando pincho en Resetear:<br>',
+    'TEXT_INFO_ADMIN_ACTIVITY_LOG' => '<strong>Tabla del Archivo de Admin vacío en la base de datos<br>¡AVISO: ASEGÚRESE DE HACER UNA COPIA DE SEGURIDAD DE LA BASE DE DATOS ANTES DE INICIAR ESTA ACTUALIZACIÓN!</strong><br>El Archivo de Actividades Admin es un método para grabar las actividades en el Admin. <br>Puede aumentar en tamaño rápidamente; hay que vaciarlo a menudo.<br>Salen avisos cuando la tabla alcanza 50.000 actividades o han pasado 60 días.<br><span class="alert">NOTA: Para cumplir con PCI, debe guardar estos datos de actividad durante 12 meses.<br>Recomendamos que guarde un copia - EXPORTAR A CSV y GUARDAR - antes de limpiar el archivo.</span>',
+    'TEXT_ADMIN_LOG_PLEASE_CONFIRM_ERASE' => '<strong><span class="alert">¡PRECAUCIÓN! Se dispone a BORRAR entradas *importantes* de las Actividades de Admin de la base de datos.</span></strong><br>Asegúrese de que tiene un backup de estas entradas ANTES de borrarlas.<br>Si continúa, ud. acepta que está borrando las entradas y que entiende sus responsabilidades legales con respecto a estas entradas.<br><br>Entiendo mis responsabilidades y quiero borrar los datos cuando pincho en Resetear:<br>',
     'SUCCESS_CLEAN_ADMIN_ACTIVITY_LOG' => 'El archivo del Actividades de Admin ha sido <strong>borrado</strong>',
     'TEXT_NO_RECORDS_FOUND' => 'No se encontró actividades utilizando el filtro que seleccionó.',
     'TEXT_EXPORTFORMAT0' => 'Exportar como HTML (para ver en pantalla)',
@@ -45,16 +45,16 @@ Debe archivar esta información por si acaso es requerida en una investigación 
 <li><strong>Severidad</strong> - Los estándares de historiales suelen definir los niveles de importancia así:
   <ul>
   <li><strong>INFO</strong> - actividades generales de administración.</li>
-  <li><strong>NOTICE</strong> - actividades que precisa un nivel de acceso mayor; puede incluir la creación de usuarios nuevos o añadir módulos de pago nuevo. También graba el envío de páginas web cuando incluye contenido potencialmente peligroso => como etiquetas script o iframe que puede haber sido añadido por empleados descontentos o intrusos. Debería revisar estos actividades a menudo.</li>
+  <li><strong>NOTICE</strong> - actividades que precisa un nivel de acceso mayor; puede incluir la creación de usuarios nuevos o añadir módulos de pago nuevo. También graba el envío de páginas web cuando incluye contenido potencialmente peligroso como etiquetas script o iframe que puede haber sido añadido por empleados descontentos o intrusos. Debería revisar estos actividades a menudo.</li>
   <li><strong>WARNING</strong> - actividades CRíTICAS como cambios en los módulos de pago o el borrado de una cuenta de un administrador que puede significar problemas urgentes. Debería revisarlos a diario.</li>
   </ul>
 </li>
-<li><strong>admin_user</strong> - Muestra el número ID de la cuenta del administrador y su nombre de usuario. Si no ha entrado en su cuenta => muestra 0.</li>
-<li><strong>page_accessed</strong> - Muestra el nombre de la página visitada => indicado el tipo de actividad.</li>
-<li><strong>parameters</strong> - Muestra los parámetros del URI de la página visitada => para ofrecer más información al tipo de actividad.</li>
-<li><strong>flagged</strong> - Si este campo está puesto a 1 => indica que debería inspeccionar el campo “postdata” para contenido no autorizado como etiquetas script o iframe. Habrá una explicación de contenido sospechoso en el campo “attention”.</li>
+<li><strong>admin_user</strong> - Muestra el número ID de la cuenta del administrador y su nombre de usuario. Si no ha entrado en su cuenta, muestra 0.</li>
+<li><strong>page_accessed</strong> - Muestra el nombre de la página visitada, indicado el tipo de actividad.</li>
+<li><strong>parameters</strong> - Muestra los parámetros del URI de la página visitada, para ofrecer más información al tipo de actividad.</li>
+<li><strong>flagged</strong> - Si este campo está puesto a 1, indica que debería inspeccionar el campo “postdata” para contenido no autorizado como etiquetas script o iframe. Habrá una explicación de contenido sospechoso en el campo “attention”.</li>
 <li><strong>attention</strong> - Muestra sugerencias relacionada al campo “postdata”.</li>
-<li><strong>logmessage</strong> - Muestra cualquier mensaje generado por el sistema sobre la actividad => como la instalación de un módulo.</li>
+<li><strong>logmessage</strong> - Muestra cualquier mensaje generado por el sistema sobre la actividad, como la instalación de un módulo.</li>
 <li><strong>postdata</strong> - Muestra los datos POST (sin el contenido sensible) para facilitar la investigación en el caso de sospechar actividad maliciosa.</li>
 </ul>',
 ];
