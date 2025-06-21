42b2530f519354e82c6611adc4a321bd367aa434
torvista <4391026+torvista@users.noreply.github.com>
2025-06-21 12:31:50 +0200

    featured categories,   install Curl

--- files/includes/languages/spanish/lang.featured_categories.php
+++ files/includes/languages/spanish/lang.featured_categories.php
@@ -2,6 +2,7 @@
 $define = [
     'NAVBAR_TITLE' => 'Categorías Destacadas',
     'HEADING_TITLE' => '%%NAVBAR_TITLE%%',
+    'TEXT_NO_FEATURED_CATEGORIES' => 'Actualmente no hay %%NAVBAR_TITLE%%.',
 ];
 
 return $define;
