cf68816a668cb7b8e14f9b08b1604aff41c96395
torvista <4391026+torvista@users.noreply.github.com>
2025-06-30 14:17:20 +0200

    fix

--- files/ADMIN_FOLDER/includes/languages/spanish/lang.password_forgotten.php
+++ files/ADMIN_FOLDER/includes/languages/spanish/lang.password_forgotten.php
@@ -17,7 +17,7 @@ $define = [
     'ERROR_WRONG_EMAIL_NULL' => 'Adiós amigo :-P',
     'MESSAGE_PASSWORD_SENT' => 'Gracias. Si el email y nombre de usuario que escribió estan asociados con alguna cuenta de administrador => una nueva contraseña se ha enviado a la cuenta.<br>Lea el email y pinche "Login" abajo para entrar con la contraseña nueva temporal.',
     'TEXT_EMAIL_SUBJECT_PWD_RESET' => 'Cambio Solicitado',
-    'TEXT_EMAIL_MESSAGE_PWD_RESET' => 'Se ha solicitado una nueva contraseña desde %1$s' . ".\n\n" . 'Su contraseña temporal es:' . "\n\n   %2$s\n\nAntes de entrar tendrá que elegir una contraseña nueva.\n\nEsta contraseña temporal caducará en 24 horas.\n\n\n",
+    'TEXT_EMAIL_MESSAGE_PWD_RESET' => 'Se ha solicitado una nueva contraseña desde %1$s' . "\n\n" . 'Su contraseña temporal es:' . "\n\n" . '%2$s' . "\n\nAntes de entrar tendrá que elegir una contraseña nueva.\n\nEsta contraseña temporal caducará en 24 horas.\n\n\n",
     'TEXT_EMAIL_SUBJECT_PWD_FAILED_RESET' => '¡Aviso de Acceso Fallado - Admin!',
     'TEXT_EMAIL_MESSAGE_PWD_FAILED_RESET' => "Han ocurrido intentos fallados del restablecimiento de contraseña de administrador de %s\n\n con email y/o nombre de usuario inválidos.\n\nSi tiene cuentas de administrador que comparten la misma dirección de email => debería asignarles direcciones de correo electrónico únicas para facilitar los restablecimientos.",
 ];
