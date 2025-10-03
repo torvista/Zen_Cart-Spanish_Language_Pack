98446a9a269e3bdd158b7dc5575b12e004558e11
torvista <4391026+torvista@users.noreply.github.com>
2025-10-03 10:47:17 +0200

    update

--- files/zc_plugins/ScanAdditionalImages/v1.0.0/admin/includes/languages/spanish/extra_definitions/lang.scan_for_additional_images.php
+++ files/zc_plugins/ScanAdditionalImages/v1.0.0/admin/includes/languages/spanish/extra_definitions/lang.scan_for_additional_images.php
@@ -1,7 +1,10 @@
 <?php // Spanish Language Pack for Zen Cart: https://github.com/torvista/Zen_Cart-Spanish_Language_Pack
 
 $define = [
-    'BOX_TOOLS_SCAN_FOR_IMAGES' => 'Escanear Imágenes Adicionales',
+    'BOX_TOOLS_SCAN_FOR_IMAGES' => 'Escanear para Imágenes Adicionales',
+
+    'SYNC_TEXT_NOTHING_TO_PROCESS' => 'No hay productos (con imágenes) para procesar en este rango. Revise los valores de Start at y tamaño.',
+    'SYNC_TEXT_QUERY_ONLY' => 'Solamente petición - sin procesar.',
 ];
 
 return $define;
