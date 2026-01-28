c0e9ca8e5a8a2b7100ce4d01176c2b2c0d97f1ca
torvista <4391026+torvista@users.noreply.github.com>
2026-01-28 12:30:57 +0100

    system inspection 1.6

--- files/zc_plugins/SystemInspection/1.5.0/Installer/languages/spanish/main.php
+++
@@ -1,9 +0,0 @@
-<?php // Spanish Language Pack for Zen Cart: https://github.com/torvista/Zen_Cart-Spanish_Language_Pack
-/**
- * @copyright Copyright 2003-2024 Zen Cart Development Team
- * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: Scott Wilson 2024 Sep 29 New in v2.1.0-beta1 $
- */
-
-define('ERROR_UNABLE_TO_DELETE_FILE', 'No se pudo borrar el archivo antiguo: %s - Habrá que borrarlo manualmente.');
-
