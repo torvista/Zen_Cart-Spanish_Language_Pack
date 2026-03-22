fc27696d31790f3ba5a9cdbf51e04fb616845ed3
torvista <4391026+torvista@users.noreply.github.com>
2026-03-22 21:58:00 +0100

    header udpates

--- files/ADMIN_FOLDER/includes/languages/spanish/lang.orders_status.php
+++ files/ADMIN_FOLDER/includes/languages/spanish/lang.orders_status.php
@@ -1,9 +1,9 @@
 <?php //https://github.com/torvista/Zen_Cart-Spanish_Language_Pack
 /**
- * @copyright Copyright 2003-2022 Zen Cart Development Team
+ * @copyright Copyright 2003-2026 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: ZenExpert 2026 Jan 14 Modified in v2.2.0-alpha1 $
+ * @version $Id: Leonard 2026 Feb 14 Modified in v2.2.1 $
  */
 
 $define = [
