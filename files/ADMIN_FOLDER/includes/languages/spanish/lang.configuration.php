87dab37ef859ecd4e7c2af9b1e0472c4646118c8
torvista <4391026+torvista@users.noreply.github.com>
2026-03-22 21:54:51 +0100

    Revert "tedious header updates for 2.2.0"
    This reverts commit 03c57968e41ffb7ab9987ac456bbfbf45237b8be.

--- files/ADMIN_FOLDER/includes/languages/spanish/lang.configuration.php
+++ files/ADMIN_FOLDER/includes/languages/spanish/lang.configuration.php
@@ -1,10 +1,10 @@
 <?php //https://github.com/torvista/Zen_Cart-Spanish_Language_Pack
 // spanish: added overrides for page titles from group name constants
 /**
- * @copyright Copyright 2003-2025 Zen Cart Development Team
+ * @copyright Copyright 2003-2022 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: lat9 2025 Sep 24 Modified in v2.2.0 $
+ * @version $Id: Scott C Wilson 2022 Jan 11 New in v1.5.8-alpha $
 */
 
 $define = [
--- files/ADMIN_FOLDER/includes/languages/spanish/lang.users.php
+++ files/ADMIN_FOLDER/includes/languages/spanish/lang.users.php
@@ -1,9 +1,9 @@
 <?php //https://github.com/torvista/Zen_Cart-Spanish_Language_Pack
 /**
- * @copyright Copyright 2003-2026 Zen Cart Development Team
+ * @copyright Copyright 2003-2024 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: lat9 2026 Feb 28 Modified in v2.2.1 $
+ * @version $Id: DrByte 2024 May 24 Modified in v2.1.0-alpha1 $
 */
 
 $define = [
